271705b16e6fc15b67aecf514b62b01fff37c37a
Usuario <Usuario@10.22.148.82>
2018-09-13 11:53:24 +0100

    V2

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,15 +1,14 @@
 package kata1;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class Kata1 {
     
     public static void main(String[] args) {
         
-        Calendar date;
-        date= GregorianCalendar.getInstance();
-        date.set(1994,6,21);
+        LocalDate date;
+        date = LocalDate.of(1994, Month.JUNE, 21);
         Person person = new Person("Javi",date);
         System.out.println(person.getName()+ " tiene " + person.getAge() 
                 + " años");
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,15 +1,15 @@
 package kata1;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.Period;
 
-import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Calendar birthdate;
+    private final LocalDate birthdate;
     private final Long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
-    public Person(String name, Calendar birthdate) {
+    public Person(String name, LocalDate birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -18,15 +18,14 @@ public class Person {
         return name;
     }
 
-    public Calendar getBirthdate() {
+    public LocalDate getBirthdate() {
         return birthdate;
     }
     
     
     public int getAge(){
-        Calendar today = GregorianCalendar.getInstance();
-        return (int) (millisecondsToYear(today.getTimeInMillis()
-                - birthdate.getTimeInMillis()));
+        LocalDate today = LocalDate.now();
+        return Period.between(birthdate,today).getYears();
     }
     
     private long millisecondsToYear(long milles){
